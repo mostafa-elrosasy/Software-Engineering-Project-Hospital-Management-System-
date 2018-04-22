818c7d1a7f25f694423b4e7e464e58f4b8d979db
Mostafa Elrosasy <mostafaelrosasy.1998@gmail.com>
2018-04-22 21:20:12 +0200

    Update Software.java

--- Software.java
+++ Software.java
@@ -1,6 +1,8 @@
 
 package software;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author mostafa
@@ -10,10 +12,15 @@ public class Software {
     /**
      * @param args the command line arguments
      */
+    static public ArrayList<patient> patientsData = new ArrayList<patient>();
+    static public String Admins[] = new String [10];
     public static void main(String[] args) {
+        
+
         mainPage P = new mainPage();
         P.setVisible(true);
     }
     
 }
+
 
